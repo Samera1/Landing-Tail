cea931df26bf1ef3064175e647926ca834df24bc
Sebastian Mera <107935758+Samera1@users.noreply.github.com>
2023-10-01 21:44:56 -0500

    Landing Page

---
+++ Landing Cloud/src/components/Button.jsx
@@ -0,0 +1,10 @@
+import styles from "../style"
+
+const Button = () =>(
+    <button type="button" className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none ${styles} rounded-[10px] mt-5`}>
+       Get Started
+    </button>
+  )
+
+
+export default Button
---
+++ Landing Cloud/src/components/index.js
@@ -0,0 +1,25 @@
+import Navbar from "./Navbar";
+import Billing from "./Billing";
+import CardDeal from "./CardDeal";
+import Bussines from "./Bussines";
+import Clients from "./Clients";
+import CTA from "./CTA";
+import Stats from "./Stats";
+import Footer from "./Footer";
+import Testimonios from "./Testimonios";
+import Hero from "./Hero";
+import Feedbackcard from "./FeedbackCard";
+
+export {
+  Navbar,
+  Billing,
+  CardDeal,
+  Bussines,
+  Clients,
+  CTA,
+  Stats,
+  Footer,
+  Testimonios,
+  Hero,
+  Feedbackcard,
+};
